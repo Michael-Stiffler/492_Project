00a3af567a18372ba622322816bd8ec04c9cf068
ctrl-dan <willsd112@gmail.com>
2022-04-11 13:14:09 -0400

    fully working nearestneighbor
    nearest neighbor is working with classification which then gets put into a regression model which tries to predict the user input and finds the closest landslide

--- groupproj.py
+++ groupproj.py
@@ -1,3 +1,4 @@
+from re import search
 import pandas as pd
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
@@ -24,13 +25,13 @@ import matplotlib.pyplot as plt # for data visualization
 #import plotly.express as px # for data visualization
 
 
-df = pd.read_csv (r'D:/Downloads/GLC03122015_stripped.csv')
+df = pd.read_csv (r'D:/Downloads/Global_Landslide_Catalog_Export_stripped.csv')
 
 lat = df["latitude"]
 lon = df["longitude"]
 
-size = df["landslide1"]
-fat = df["fatalities"]
+size = df["landslide_size"]
+fat = df["fatality_count"]
 
 lat = lat.to_numpy()
 lon = lon.to_numpy()
@@ -40,7 +41,7 @@ test = [23.2,79.3]
 coord = np.column_stack((lon, lat))
 #coord = (lon, lat)
 
-nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(coord)
+nbrs = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(coord)
 print(nbrs)
 distances, indices = nbrs.kneighbors(coord)
 
@@ -51,22 +52,22 @@ distances, indices = nbrs.kneighbors(coord)
 
 
 
-landslide1 = df["landslide1"]
+landslide1 = df["landslide_size"]
 landslide1 = landslide1.to_numpy()
 size_array = []
 fat_array = []
 for word in landslide1:
    # print(word)
-    if word == 'Small':
+    if word == 'small':
         word = 0
         size_array.append(word)
-    elif word == 'Medium':
+    elif word == 'medium':
         word = 2
         size_array.append(word)
-    elif word == 'Large':
+    elif word == 'large':
         word = 4
         size_array.append(word)
-    elif word == 'Very_large':
+    elif word == 'very_large':
         word = 9
         size_array.append(word)
     elif word == 'unknown':
@@ -75,6 +76,25 @@ for word in landslide1:
     else:
         word = 0
         size_array.append(word)
+
+size_array2 = []
+for word in landslide1:
+   # print(word)
+    if word == 0:
+        word = 0
+        size_array2.append(word)
+    elif word == 1:
+        word = 2
+        size_array2.append(word)
+    elif word == 2:
+        word = 4
+        size_array2.append(word)
+    elif word == 3:
+        word = 7
+        size_array2.append(word)
+    else:
+        word = 0
+        size_array2.append(word)
 
 for count in fat:
    # print(word)
@@ -88,10 +108,10 @@ for count in fat:
         word = 3
         fat_array.append(word)
     elif count > 11 and count < 31:
-        word = 7
+        word = 5
         fat_array.append(word)
     elif count > 31:
-        word = 9
+        word = 7
         fat_array.append(word)
     else:
         word = 0
@@ -131,6 +151,35 @@ for i in class_array:
 #print(classification)
 #print(coord)
 
+
+from csv import writer
+from csv import reader
+
+#print(len(classification))
+classification2 = classification
+classification2.insert(0, "classification")
+
+
+# Open the input_file in read mode and output_file in write mode
+with open('D:/Downloads/Global_Landslide_Catalog_Export_stripped.csv', encoding="utf8") as read_obj, \
+        open('D:/Downloads/Global_Landslide_Catalog_Export2.csv', 'w', newline='') as write_obj:
+    # Create a csv.reader object from the input file object
+    csv_reader = reader(read_obj)
+    # Create a csv.writer object from the output file object
+    csv_writer = writer(write_obj)
+    # Read each row of the input csv file as list
+    count = 0
+    for row in csv_reader:
+        #print(count)
+        # Append the default text in the row / list
+        #print(classification[count])
+        temp = classification[count]
+        row.append(temp)
+        count = count + 1
+        # Add the updated row / list to the output file
+        csv_writer.writerow(row)
+
+
 K = 10
 model = KNeighborsClassifier(n_neighbors = K)
 #model.fit(coord, classification)
@@ -140,17 +189,18 @@ print()
 #print( '(32,77) is class'),
 #print( model.predict([[32,77]]) )
 
+df2 = pd.read_csv (r'D:/Downloads/Global_Landslide_Catalog_Export2.csv')
 
 
 # Do Min-Max scaling
 scaler = MinMaxScaler()
-df['fatalities scl']=scaler.fit_transform(df[['fatalities']])
-df['landslide1 scl']=scaler.fit_transform(df[['landslide1']])
-df['latitude scl']=scaler.fit_transform(df[['latitude']])
-df['longitude scl']=scaler.fit_transform(df[['longitude']])
+#df2['fatalities scl']=scaler.fit_transform(df2[['fatality_count']])
+#df2['landslide1 scl']=scaler.fit_transform(df2[['landslide_size']])
+df2['latitude scl']=scaler.fit_transform(df2[['latitude']])
+df2['longitude scl']=scaler.fit_transform(df2[['longitude']])
 
 # Split into train and test dataframes
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+df_train, df_test = train_test_split(df2, test_size=0.2, random_state=42)
 
 # Independent variables (features)
 #X_train=df_train[['fatalities scl', 'landslide1 scl', 'latitude scl','longitude scl']]
@@ -165,12 +215,12 @@ print(X_test2)
 #yC_train=df_train['Price Band enc'].ravel()
 #yC_test=df_test['Price Band enc'].ravel()
 # Target for regression model
-yR_train=df_train['class'].ravel()
-yR_test=df_test['class'].ravel()
+yR_train=df_train['classification'].ravel()
+yR_test=df_test['classification'].ravel()
 
 
 
-modelR = KNeighborsRegressor(n_neighbors=3, #default=5
+modelR = KNeighborsRegressor(n_neighbors=4, #default=5
                              weights='uniform', #{‘uniform’, ‘distance’} or callable, default='uniform'
                              algorithm='auto', #{‘auto’, ‘ball_tree’, ‘kd_tree’, ‘brute’}, default=’auto’
                              #leaf_size=30, #default=30, Leaf size passed to BallTree or KDTree.
@@ -241,8 +291,67 @@ print('---------------------------------------------------------')
 
 
 
-print( '(25,96) is class'),
-print( modelR.predict([[25.6,96]]) )
+def assess(result):
+    print(result[0])
+    if result[0] <= 0.4999999:
+        print("low risk")
+    elif result[0] >= 0.5 and result[0] <= 1.19999999:
+        print("medium risk")
+    elif result[0] >= 1.2:
+        print("high risk")
 
-print( '(-43.5,170) is class'),
-print( modelR.predict([[-43.5,170]]) )
+
+
+lat_in = input("Enter latitude : ")
+long_in = input ("Enter longitude :")
+
+
+print("(",lat_in,",",long_in,") is a "),
+#print( modelR.predict([[25.6,96]]) )
+result =  modelR.predict([[lat_in,long_in]])
+assess(result)
+
+lat_in = float(lat_in)
+long_in = float(long_in)
+
+# Example of getting neighbors for an instance
+from math import sqrt
+ 
+# calculate the Euclidean distance between two vectors
+def euclidean_distance(row1, row2):
+    distance = 0.0
+    x = np.array2string(row2)
+    #print(x)
+    x = x[1:-1]
+    temp = []
+    temp = x.split()
+    #print(temp)
+    for i in range(len(row1)-1):
+        temp2 = float(temp[i])
+        distance += (row1[i] - temp2)**2
+    return sqrt(distance)
+
+# Locate the most similar neighbors
+def get_neighbors(train, test_row, num_neighbors):
+	distances = list()
+	for train_row in train:
+		dist = euclidean_distance(test_row, train_row)
+		distances.append((train_row, dist))
+	distances.sort(key=lambda tup: tup[1])
+	neighbors = list()
+	for i in range(num_neighbors):
+		neighbors.append(distances[i][0])
+	return neighbors
+
+
+for row in coord:
+    distance = euclidean_distance([lat_in,long_in], row)
+    #print(row)
+    #print(distance)
+
+    
+neighbors = get_neighbors(coord, [lat_in,long_in], 1)
+for neighbor in neighbors:
+	print(neighbor)
+
+
